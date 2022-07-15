66048d30683048bd25b535798844194dd1f8be58
Divya gavandi <49062060+gavandivya@users.noreply.github.com>
2022-07-15 19:17:28 +0530

    feat:initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef, useState } from "react";
 
-function App() {
+export default function App() {
+
+  var birthdate = useRef(0);
+  var luckynumber = useRef(0);
+  var [BirthdayMessage,setMessage] = useState('');
+  var [errorMessage,setError] = useState('');
+
+  function checkLucky(){
+    const birthDate = birthdate.current.value;
+    const luckyNumber = luckynumber.current.value;
+    let sum = 0;
+    if(birthDate && luckyNumber){
+      if(luckyNumber > 0){
+        sum = calculateSum(birthDate);
+        if(sum == luckyNumber){
+          setMessage("Your Birthday is lucky");
+        }
+      else{
+        setMessage("Your Birthday is NOT lucky, so sad");
+      }
+      }
+      else{
+        setError("Please enter positive number");
+      }}
+      else{
+        setError("Please fill the details");
+      }
+    // console.info(sum);
+    // console.info(typeof sum);
+    // console.info(typeof luckyNumber);
+  }
+
+  function calculateSum(birthDate){
+    birthDate = birthDate.replaceAll("-","");
+    // console.info(birthDate);
+    let sum = 0;
+    while(birthDate !== 0){
+      sum = sum + birthDate % 10;
+      birthDate = Math.floor(birthDate / 10);
+    }
+    // for(let i = 0; i < birthDate.length;i++){
+    //   sum = sum + Number(birthDate.charAt(i));
+    // }
+    return sum;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>Is your birthday lucky?🥳</h4>
+      <div className='borderClass'>
+      <div>
+        <label>Date of Birth : </label>
+        <input type="date" ref={birthdate} />
+      </div>
+      <div>
+        <label>Lucky Number : </label>
+        <input type="number" placeholder='Enter your Lucky Number' ref={luckynumber} />
+      </div>
+      <div style={{textAlign:"center"}}>
+      <button onClick={checkLucky}>Check Number</button>
+      <br></br><span style={{color:"red", fontSize:"12px"}}>{errorMessage}</span>
+      <p>{BirthdayMessage}</p>
+      </div>
+      </div>
     </div>
   );
 }
-
-export default App;
