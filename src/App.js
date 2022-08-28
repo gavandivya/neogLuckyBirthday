c228d2bbcfc09644e082c8358cf31a54ef04d727
Divya gavandi <49062060+gavandivya@users.noreply.github.com>
2022-08-28 22:37:41 +0530

    fix:formulaChanged

--- src/App.js
+++ src/App.js
@@ -5,38 +5,39 @@ export default function App() {
 
   var birthdate = useRef(0);
   var luckynumber = useRef(0);
-  var [BirthdayMessage,setMessage] = useState('');
-  var [errorMessage,setError] = useState('');
+  var [BirthdayMessage, setMessage] = useState('');
+  var [errorMessage, setError] = useState('');
 
-  function checkLucky(){
+  function checkLucky() {
     setError("");
     setMessage("");
     const birthDate = birthdate.current.value;
     const luckyNumber = luckynumber.current.value;
     let sum = 0;
-    if(birthDate && luckyNumber){
-      if(luckyNumber > 0){
+    if (birthDate && luckyNumber) {
+      if (luckyNumber > 0) {
         sum = calculateSum(birthDate);
-        if(sum === Number(luckyNumber)){
+        if (sum % Number(luckyNumber) === 0) {
           setMessage("Your Birthday is lucky");
         }
-      else{
-        setMessage("Your Birthday is NOT lucky, so sad");
+        else {
+          setMessage("Your Birthday is NOT lucky, so sad");
+        }
       }
-      }
-      else{
+      else {
         setError("Please enter positive number");
-      }}
-      else{
-        setError("Please fill the details");
       }
+    }
+    else {
+      setError("Please fill the details");
+    }
   }
 
-  function calculateSum(birthDate){
-    birthDate = birthDate.replaceAll("-","");
+  function calculateSum(birthDate) {
+    birthDate = birthDate.replaceAll("-", "");
     // console.info(birthDate);
     let sum = 0;
-    while(birthDate !== 0){
+    while (birthDate !== 0) {
       sum = sum + birthDate % 10;
       birthDate = Math.floor(birthDate / 10);
     }
@@ -50,21 +51,21 @@ export default function App() {
     <div className="App">
       <h4>Is your birthday lucky?🎂🥳</h4>
       <div className='borderClass'>
-      <div>
-        <label>Date of Birth</label>
-        <input type="date" ref={birthdate} />
+        <div>
+          <label>Date of Birth</label>
+          <input type="date" ref={birthdate} />
+        </div>
+        <div>
+          <label>Lucky Number </label>
+          <input type="number" placeholder='Enter your Lucky Number' ref={luckynumber} />
+        </div>
+        <div style={{ textAlign: "center" }}>
+          <button onClick={checkLucky}>Check Number</button>
+          <br></br><span style={{ color: "red", fontSize: "12px" }}>{errorMessage}</span>
+          <p>{BirthdayMessage}</p>
+        </div>
       </div>
-      <div>
-        <label>Lucky Number </label>
-        <input type="number" placeholder='Enter your Lucky Number' ref={luckynumber} />
-      </div>
-      <div style={{textAlign:"center"}}>
-      <button onClick={checkLucky}>Check Number</button>
-      <br></br><span style={{color:"red", fontSize:"12px"}}>{errorMessage}</span>
-      <p>{BirthdayMessage}</p>
-      </div>
-      </div>
-      <footer style={{position:"fixed",bottom:0}}>
+      <footer style={{ position: "fixed", bottom: 0 }}>
         <ul>
           <li><a href="https://www.linkedin.com/in/divya-gavandi/"><i className="fa fa-linkedin"></i></a></li>
           <li><a href="https://www.facebook.com/divya.gavandi.3"><i className="fa fa-facebook" aria-hidden="true"></i> </a></li>
